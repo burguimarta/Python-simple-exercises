6bc1ef773fcf6ef8f47e864cb09bb85cebc4fdfd
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-10-27 11:34:36 +0200

    first commit
    Done exercises to learn Python and improve my logic

---
+++ Ejercicio 1.py
@@ -0,0 +1,5 @@
+
+nombre = input("Dime tu nombre: ");
+edad = input("Dime tu edad: ");
+
+print("Hola " + nombre + ", tienes " + edad + " años");
---
+++ Ejercicio 2.py
@@ -0,0 +1,2 @@
+if value == 1900:
+        print("Eres mayor de edad")
